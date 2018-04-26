efc72bc0daedc458ba583762ae4b0ecfd5c69da2
sergeymitrichev <sergeymitrichev@yandex.ru>
2018-04-26 10:37:11 +0400

    Add product entity

---
+++ core/src/main/java/ru/minutka_nn/model/Product.java
@@ -0,0 +1,31 @@
+package java.ru.minutka_nn.model;
+
+import java.util.List;
+
+public class Product {
+
+    private int id;
+
+    private String name;
+
+    private String sku;
+
+    private String brand;
+
+    private String brief;
+
+    private String description;
+
+    private List<String> images;
+
+    private int soldPrice;
+
+    private int incomePrice;
+
+    private int oldPrice;
+
+    private int weight;
+
+    private List<Product> recommended;
+
+}
